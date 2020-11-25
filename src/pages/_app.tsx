ea2dceaa7d5aaf035cc21081befb2d83032a4c88
kbbushman <k.bushman@yahoo.com>
2020-11-25 13:57:09 -0800

    Adds logout to flush urql me cache on logout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ import {
 } from 'urql';
 import {
   LoginMutation,
+  LogoutMutation,
   MeDocument,
   MeQuery,
   RegisterMutation
@@ -34,6 +35,14 @@ const client = createClient({
     cacheExchange({
       updates: {
         Mutation: {
+          logout: (_result, args, cache, info) => {
+            betterUpdateQuery<LogoutMutation, MeQuery>(
+              cache,
+              {query: MeDocument},
+              _result,
+              () => ({me: null})
+            )
+          },
           login: (_result, args, cache, info) => {
             // cache.updateQuery({query: MeDocument}, (data: MeQuery) => {});
             betterUpdateQuery<LoginMutation, MeQuery>(
