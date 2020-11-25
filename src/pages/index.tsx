6ca7734dac5fd08460a145a6e4d71055b6fe8a18
kbbushman <k.bushman@yahoo.com>
2020-11-25 15:15:04 -0800

    Adds posts query and withUrqlClient hoc for server side posts rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { withUrqlClient } from 'next-urql';
 import NavBar from "../components/NavBar";
+import { createUrqlClient } from '../utils/createUrqlClient';
+import { usePostsQuery } from "../generated/graphql";
 
-const Index = () => (
-  <>
-    <NavBar />
-    <div>Hello Dave...</div>
-  </>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
 
-export default Index;
+  return (
+    <>
+      <NavBar />
+      <div>Hello Dave...</div>
+      {!data ? (<div>Loading...</div>) : data.posts.map((post) => (
+        <div key={post.id}>
+          {post.title}
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default withUrqlClient(createUrqlClient, {ssr: true})(Index);
