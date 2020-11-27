088dce267c6d5d51adc4fd388e5d5c7110095a72
kbbushman <k.bushman@yahoo.com>
2020-11-26 19:30:53 -0800

    Adds forgot password page with mutation

---
+++ src/pages/forgot-password.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Box, Button } from '@chakra-ui/react';
+import { Formik, Form } from 'formik';
+import InputField from '../components/InputField';
+import Wrapper from '../components/Wrapper';
+import { createUrqlClient } from '../utils/createUrqlClient';
+import { withUrqlClient } from 'next-urql';
+import { useForgotPasswordMutation } from '../generated/graphql';
+
+const ForgotPassword: React.FC<{}> = ({}) => {
+  const [complete, setComplete] = useState(false);
+  const [, forgotPassword] = useForgotPasswordMutation();
+  
+  return (
+    <Wrapper variant='small'>
+      <Formik
+        initialValues={{email: ''}}
+        onSubmit={async (values) => {
+          await forgotPassword(values);
+          setComplete(true);
+        }}
+      >
+        {({ isSubmitting }) => complete
+          ? <Box>If an account with that email exists, we sent you an email</Box>
+          : (
+              <Form>
+                <InputField
+                  name='email'
+                  placeholder='email'
+                  label='Email'
+                  type='email'
+                />
+                <Button
+                  mt={4}
+                  type='submit'
+                  colorScheme='teal'
+                  isLoading={isSubmitting}
+                >
+                  Send Password Reset Link
+                </Button>
+              </Form>
+            )
+          }
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default withUrqlClient(createUrqlClient)(ForgotPassword);
