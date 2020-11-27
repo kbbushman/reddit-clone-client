acb00548c92a0bd0dbd99614a8352f6f84cccd36
kbbushman <k.bushman@yahoo.com>
2020-11-27 14:39:04 -0800

    Adds optional textarea prop, checks for prop to change input component to textarea

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -5,19 +5,32 @@ import {
   FormLabel,
   Input,
   FormErrorMessage,
-} from '@chakra-ui/react';
+  Textarea,
+} from '@chakra-ui/core'; // @charkra-ui/react input prop types do not match with Input and Textarea. Must use @charkra-ui/core
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   name: string;
   label: string;
+  textarea?: boolean;
 };
 
-const InputField: React.FC<InputFieldProps> = ({ label, size: _, ...props }) => {
+const InputField: React.FC<InputFieldProps> = ({
+  label,
+  textarea,
+  size: _,
+  ...props
+}) => {
+  let InputOrTextarea = Input;
+
+  if (textarea) {
+    InputOrTextarea = Textarea;
+  }
+
   const [field, { error }] = useField(props);
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input {...field} {...props} id={field.name} placeholder={props.placeholder} />
+      <InputOrTextarea {...field} {...props} id={field.name} placeholder={props.placeholder} />
       {error && <FormErrorMessage>{error}</FormErrorMessage>}
     </FormControl>
   );
