767125cf0d0292d95f1e7bb1ff02cd69734df52d
kbbushman <k.bushman@yahoo.com>
2020-11-27 23:14:19 -0800

    Adds home page header and wraps posts in cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import NextLink from 'next/link';
-import { Link } from '@chakra-ui/react';
+import { Box, Flex, Heading, Link, Stack, Text } from '@chakra-ui/react';
 import { withUrqlClient } from 'next-urql';
 import { createUrqlClient } from '../utils/createUrqlClient';
 import { usePostsQuery } from '../generated/graphql';
 import Layout from '../components/Layout';
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
+  const [{ data, fetching }] = usePostsQuery({
     variables: {
       limit: 10,
     }
@@ -15,17 +15,28 @@ const Index = () => {
 
   return (
     <Layout>
-      <NextLink href='/create-post'>
-        <Link>
-          Create Post
-        </Link>
-      </NextLink>
+      <Flex align='center'>
+        <Heading>RedditClone</Heading>
+        <NextLink href='/create-post'>
+          <Link ml='auto'>
+            Create Post
+          </Link>
+        </NextLink>
+      </Flex>
       <br />
-      {!data ? (<div>Loading...</div>) : data.posts.map((post) => (
-        <div key={post.id}>
-          {post.title}
-        </div>
-      ))}
+      {fetching && !data 
+        ? <div>Loading...</div>
+        : (
+          <Stack spacing={8}>
+            {data?.posts.map((post) => (
+              <Box key={post.id} p={5} shadow="md" borderWidth="1px">
+                <Heading fontSize="xl">{post.title}</Heading>
+                <Text mt={4}>{post.textSnippet}</Text>
+              </Box>
+            ))}
+          </Stack>
+        )
+      }
     </Layout>
   );
 };
