07125eb85f15534ddca88ce948f57dc648e62e21
kbbushman <k.bushman@yahoo.com>
2020-11-27 22:28:01 -0800

    Updates home page posts query to include post limit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,11 @@ import { usePostsQuery } from '../generated/graphql';
 import Layout from '../components/Layout';
 
 const Index = () => {
-  const [{ data }] = usePostsQuery();
+  const [{ data }] = usePostsQuery({
+    variables: {
+      limit: 10,
+    }
+  });
 
   return (
     <Layout>
