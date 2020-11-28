c7365b6ff70ddb6a62ca1fbe0451ba1826791c90
kbbushman <k.bushman@yahoo.com>
2020-11-27 23:59:38 -0800

    Adds urql pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import NextLink from 'next/link';
 import { Box, Button, Flex, Heading, Link, Stack, Text } from '@chakra-ui/react';
 import { withUrqlClient } from 'next-urql';
@@ -7,10 +7,9 @@ import { usePostsQuery } from '../generated/graphql';
 import Layout from '../components/Layout';
 
 const Index = () => {
+  const [variables, setVariables] = useState({limit: 10, cursor: null as string | null});
   const [{ data, fetching }] = usePostsQuery({
-    variables: {
-      limit: 10,
-    }
+    variables,
   });
 
   if (!fetching && !data) {
@@ -49,6 +48,10 @@ const Index = () => {
             my={8}
             colorScheme='blue'
             isLoading={fetching}
+            onClick={() => {console.log('click'); setVariables({
+              limit: variables.limit,
+              cursor: data.posts[data.posts.length - 1].createdAt})
+            }}
           >
             Load More
           </Button>
