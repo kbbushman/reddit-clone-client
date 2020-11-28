e478c76d04bb90dde392f1beb2b1e9da551824f9
kbbushman <k.bushman@yahoo.com>
2020-11-27 16:24:22 -0800

    Adds Layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,28 @@
 import React from "react";
+import NextLink from 'next/link';
+import { Link } from "@chakra-ui/react";
 import { withUrqlClient } from 'next-urql';
-import NavBar from "../components/NavBar";
 import { createUrqlClient } from '../utils/createUrqlClient';
-import { usePostsQuery } from "../generated/graphql";
+import { usePostsQuery } from '../generated/graphql';
+import Layout from '../components/Layout';
 
 const Index = () => {
   const [{ data }] = usePostsQuery();
 
   return (
-    <>
-      <NavBar />
-      <div>Hello Dave...</div>
+    <Layout>
+      <NextLink href='/create-post'>
+        <Link>
+          Create Post
+        </Link>
+      </NextLink>
+      <br />
       {!data ? (<div>Loading...</div>) : data.posts.map((post) => (
         <div key={post.id}>
           {post.title}
         </div>
       ))}
-    </>
+    </Layout>
   );
 };
 
