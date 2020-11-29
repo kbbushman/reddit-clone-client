0624791d274821de50b43cbc4da6434204df3239
kbbushman <k.bushman@yahoo.com>
2020-11-28 20:16:41 -0800

    Adds urql client vote fragment

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -1,4 +1,5 @@
 import Router from 'next/router';
+import gpl from 'graphql-tag';
 import { dedupExchange, fetchExchange, Exchange, stringifyVariables } from "urql";
 import { cacheExchange, Resolver } from '@urql/exchange-graphcache';
 import { pipe, tap } from 'wonka';
@@ -7,7 +8,9 @@ import {
   MeQuery,
   MeDocument,
   LoginMutation,
-  RegisterMutation
+  RegisterMutation,
+  VoteMutation,
+  VoteMutationVariables
 } from "../generated/graphql";
 import { betterUpdateQuery } from "./betterUpdateQuery";
 
@@ -77,6 +80,29 @@ export const createUrqlClient = (ssrExchange: any) => ({
       },
       updates: {
         Mutation: {
+          vote: (_result, args, cache, info) => {
+            const { postId, value } = args as VoteMutationVariables;
+            const data = cache.readFragment(
+              gpl`
+                fragment _ on Post {
+                  id
+                  points
+                }
+              `,
+              {id: postId} as any
+            );
+            if (data) {
+              const newPoints = (data.points as number) + value;
+              cache.writeFragment(
+                gpl`
+                  fragment __ on Post {
+                    points
+                  }
+                `,
+                {id: postId, points: newPoints} as any
+              );
+            }
+          },
           createPost: (_result, args, cache, info) => {
             const allFields = cache.inspectFields('Query');
             const fieldInfos = allFields.filter((info) => info.fieldName === 'posts');
