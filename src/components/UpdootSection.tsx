4fd01fd3bf0b8bcce942d65593f25cbd93ed97d7
kbbushman <k.bushman@yahoo.com>
2020-11-28 17:13:09 -0800

    Adds vote mutation to upvote/downvote buttons

--- src/components/UpdootSection.tsx
+++ src/components/UpdootSection.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Flex, IconButton } from '@chakra-ui/react';
 import { ChevronUpIcon, ChevronDownIcon } from '@chakra-ui/icons';
-import { PostSnippetFragment } from '../generated/graphql';
+import { PostSnippetFragment, useVoteMutation } from '../generated/graphql';
 
 interface UpdootSectionProps {
   post: PostSnippetFragment;
 }
 
 const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
+  const [, vote] = useVoteMutation();
   return (
     <Flex
       direction='column'
@@ -18,13 +19,17 @@ const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
       <IconButton
         aria-label="Upvote"
         icon={<ChevronUpIcon size='24px' />}
-        onClick={() => console.log('Upvote ' + post.id)}
+        onClick={() => {
+          vote({postId: post.id, value: 1})
+        }}
       />
       {post.points}
       <IconButton
         aria-label="Downvote"
         icon={<ChevronDownIcon size='24px' />}
-        onClick={() => console.log('Downvote ' + post.id)}
+        onClick={() => {
+          vote({postId: post.id, value: -1})
+        }}
       />
     </Flex>
   );
