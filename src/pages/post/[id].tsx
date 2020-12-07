9e5dd0f8fe35c7e2a4254ef98b9eab6a30c70ed5
kbbushman <k.bushman@yahoo.com>
2020-12-06 22:40:43 -0800

    Adds getPost query and post title/text

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,19 +1,50 @@
 import React from 'react';
 import { withUrqlClient } from 'next-urql';
 import { useRouter } from 'next/router';
+import { Box, Heading } from '@chakra-ui/react';
 import { createUrqlClient } from '../../utils/createUrqlClient';
+import { usePostQuery } from '../../generated/graphql';
+import Layout from '../../components/Layout';
 
-interface PostProps {
-
-}
-
-const Post: React.FC<PostProps> = ({}) => {
+const Post = ({}) => {
   const router = useRouter();
-  router.query.id;
+  const intId = typeof router.query.id === 'string' ? parseInt(router.query.id) : -1;
+  const [{data, error, fetching}] = usePostQuery({
+    pause: intId === -1,
+    variables: {
+      id: intId
+    }
+  });
+
+  if (fetching) {
+    return (
+      <Layout>
+        <div>Loading...</div>
+      </Layout>
+    );
+  }
+
+  if (error) {
+    return (
+      <Layout>{error.message}</Layout>
+    );
+  }
+
+  if (!data?.post) {
+    return (
+      <Layout>
+        <Box>
+          Could not find post
+        </Box>
+      </Layout>
+    );
+  }
+
   return (
-    <div>
-      Post Detail
-    </div>
+    <Layout>
+      <Heading>{data?.post?.title}</Heading>
+      {data?.post?.text}
+    </Layout>
   );
 };
 
